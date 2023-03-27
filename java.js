9b742c262ca76821a1a4ae2227abf37cfca83caf
Naitik-ag <agarwalnaitikslg@gmail.com>
2023-03-27 23:59:41 +0530

    unlimired chance

--- java.js
+++ java.js
@@ -1,57 +1,67 @@
 
 
-let a = ['rock','paper','scissors'];
-let compChoice= a[Math.floor(Math.random()*3)];
+let buttonChoice;
+const scrchoice = document.querySelector('.playchoice')
+const cmpscrchoice =  document.querySelector('.compchoice')
+const result = document.querySelector('.result')
+const playscore = document.querySelector('.playscore')
+const srccompscore = document.querySelector('.compscore')
+let playerScore =0;
+let compScore =0;
 
 
-function play(){
-    inp = prompt("ENter");
-    playerChoice= inp.toLowerCase();
+const ROCK = document.querySelector('.rock')
+ROCK.addEventListener('click', () => {
+    let buttonChoice = "rock";
+    scrchoice.textContent = `Your Choice: ${buttonChoice.toUpperCase()}`
+    result.textContent = play(buttonChoice)
+})
 
-    if(playerChoice==compChoice)
+
+const PAPER = document.querySelector('.paper');
+PAPER.addEventListener('click', () => {
+    let buttonChoice = "paper";
+    scrchoice.textContent = `Your Choice: ${buttonChoice.toUpperCase()}`
+    result.textContent = play(buttonChoice)
+});
+
+const SCISSORS = document.querySelector('.scissors');
+SCISSORS.addEventListener('click', () => {
+    let buttonChoice = "scissors";
+    scrchoice.textContent = `Your Choice: ${buttonChoice.toUpperCase()}`
+    result.textContent = play(buttonChoice)
+});
+
+
+
+function play(buttonChoice){
+    let a = ['rock','paper','scissors'];
+    let compChoice= a[Math.floor(Math.random()*3)];
+    cmpscrchoice.textContent = `Computer choice: ${compChoice.toUpperCase()}`
+    
+    if(buttonChoice==compChoice)
     {
-        return "It is tie";
+        return("It's A Tie!! No Big Deal");
     }
 
-    else if(playerChoice== 'paper')
-    {
-        if (compChoice=='rock')
-        {
-            return "You win";
-        }    
-
-        else{
-            return "You lose";
-        } 
+    if((buttonChoice=='paper'&& compChoice== 'rock')||(buttonChoice=='rock'&& compChoice== 'scissors')||(buttonChoice=='scissors'&& compChoice== 'paper')){
+        ++playerScore
+        playscore.textContent = `Player Score: ${playerScore}`
+        return("HaHa , AI Loses")
+        
+    }
+    else{
+        ++compScore
+        srccompscore.textContent = `Computer Score: ${compScore}`
+        return("Dammit!! AI Wins")
+        
     }
 
-    else if(playerChoice== 'scissors')
-    {
-        if (compChoice=='paper')
-        {
-            return "You win";
-        }   
 
-        else
-        {
-            return "You lose";
-        } 
-    }
-
-    else if(playerChoice== 'rock')
-    {
-        if (compChoice=='scissors')
-        {
-            return "You win";
-        }  
-
-        else{
-            return "You lose";
-        } 
-    }
 }
 
 
+/*
 function round5()
 {
     for(let i=0 ; i <5;i++)
@@ -59,3 +69,8 @@ function round5()
         console.log(play());
     }
 }
+*/
+
+
+
+
